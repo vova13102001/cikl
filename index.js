e045c59d7aa8b8ccb2918d55895bdac4622edd24
vova13102001 <74361325+vova13102001@users.noreply.github.com>
2021-01-30 19:56:44 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,110 @@
+//задание 1.1
+
+/*for(var i=25;i>=0;i--) {
+alert(i);
+  }
+ 
+  //задание 1.2
+
+  let q = 25;
+while (q >= 0) { 
+  alert( q );
+  q--;
+}
+
+//задание 1.3
+
+let w = 25;
+do {
+  alert( w );
+  w--;
+} while (w >= 0);*/
+
+// pflfybt 2.1
+/*
+ let tempArr = [];
+
+for (let i = 10;  i <= 50; i++) {
+  tempArr.push((i % 5 === 0 ) ? `${i}` : i);
+}
+
+function qwerty(arr) {
+  arr.forEach(i => console.log(i))
+}
+
+qwerty(tempArr);
+
+//задание 2.2
+
+let n = 10;
+while( ++n <= 50){
+ console.log( ( n % 5 === 0) ? n.toString() : n);
+ 
+}
+//задание 2.3 
+
+function asd() {
+  let num = 10;
+  
+  do {
+
+   console.log( ( num % 5 === 0 ) ? num.toString() : num);
+   
+  } while (++num <= 50 );
+}
+  asd()
+
+// задние 3.1
+
+function sumTo(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+alert( sumTo(100) );
+*/
+// задание 3.2?????????????????????????????????????????????????????????????
+/*
+let n = 100;
+let sum = 0;
+
+while (n > 0) {
+    sum += n % 10
+    n = Math.floor(n/10)
+}
+alert (sum)*/
+//задание 3.3
+
+
+
+//задание 4.1 ???????????????????????????????????
+
+
+ 
+function sumTo() {
+  let sum = 0;
+  for ( i++) {
+    sum += i;
+  }
+  
+}
+
+sumTo() 
+
+//задание 4.2
+
+//задание 4.3
+/*
+function sumTo() {
+let num;
+
+do {
+ 
+  num = prompt("Введите результат ( 2 + 2 * 2 )", 0) ;
+
+} while (num != 6 );
+}
+sumTo()*/
